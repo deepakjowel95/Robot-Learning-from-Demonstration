d1e945cb880470fae7fbb9efe817de0a87f436da
deepakjowel95 <51018258+deepakjowel95@users.noreply.github.com>
2025-03-01 13:32:15 +0000

    Initial commit

---
+++ pydmps/pydmps/examples/workshop.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error
+import pydmps
+import pydmps.dmp_discrete
+
+
+### Load a demonstration trajectory as numpy array from the examples folder  #######
+
+
+### Create the DMP object and set number of DMPs, RBFs and Starting state #######
+
+
+### Train the weights of the DMPs using the imitate path method ###
+
+### generate a trajectory from the trainned DMP using the rollout method ####
+
+
+### Use Matplotlib to generate the desired plots ####
